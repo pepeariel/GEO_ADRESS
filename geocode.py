c94326b6d65346d11215af60894a655099b0049d
pepeariel <peedroariel@gmail.com>
2023-01-27 14:23:29 -0300

    distance_calculation

--- geocode.py
+++ geocode.py
@@ -1,6 +1,11 @@
 import requests
+import math
+import time
 
 SECRET_API_KEY = 'AIzaSyApe4spP6T-B8z-RgNrs6SuNDDNdMdvLRQ='
+
+origin = str(input('Digite o local de origem:'))
+destination = str(input('Digite o local de destino:'))
 
 def get_lat_lng(address):
     url = 'https://maps.googleapis.com/maps/api/geocode/json'
@@ -24,11 +29,30 @@ def get_address(lat, lng):
     else:
         return None
     
-# Get the latitude and longitude based on the adress
-lat, lng = get_lat_lng('1600 Amphitheatre Parkway, Mountain View, CA')
-print(f'Latitude: {lat}, Longitude: {lng}')
+def distance_between_coordinates(lat1, lon1, lat2, lon2):
+    earth_radius = 6371 # Earth's radius in kilometers
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = earth_radius * c
+    return distance
 
-# Get the adress base on latitude and longitude
-lat, lng = '-33.8688' , '151.2093'
-address = get_address(lat, lng)
-print(address)
+
+# Get the latitude and longitude based on the origin
+lat1, lon1 = get_lat_lng(origin)
+
+# Await
+time.sleep(3)
+print('calculando..')
+
+# Get the latitude and longitude based on the destination
+lat2, lon2 = get_lat_lng(destination)
+
+# Get the distance between origin and destination
+distance = distance_between_coordinates(lat1, lon1, lat2, lon2)
+print(f'Distancia total: {distance} Km')
